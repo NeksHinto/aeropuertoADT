99486888fa5ea6f7632f2717f5bb51022e1e6a2a
valepiombi <40894353+valepiombi@users.noreply.github.com>
2018-07-07 22:41:03 -0300

    Update aeropuertoBack.c

--- aeropuertoBack.c
+++ aeropuertoBack.c
@@ -106,16 +106,24 @@ tLista agregaAeropuertoRec(tLista listaAeropuerto, const char OACI[], const char
 	return listaAeropuerto;
 }
 
-int movAeropuerto(tLista lista){
-	FILE *fd;
-	fd=fopen("movs_aeropuerto.csv", "w");
-	tLista l=lista;
+int movAeropuerto(tLista listaDeAeropuertosLocales){
+	FILE * archivoP; //puntero que apunta a un archivo 
+	fd=fopen("movs_aeropuerto.csv", "w"); //crea un archivo nombre "movs_aeropuerto(...)" y recibe un string "w" que permite que se escriba en el archivo creado
+	tLista l=listaDeAeropuertosLocales;
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/*Recorremos toda la lista de aeropuertos locales y unicamente si el aeropuerto tuvo movimiento copiamos
+	los datos al archivo creado*////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	while(l!=NULL){
 		if((l->aeropuerto->movimiento->totalDespegues !=0) || (l->aeropuerto->movimiento->totalAterrizajes !=0)) //NO debe contener aeropuertos con cero movimientos en el año pedido
 			fprintf(fd, "%s;%s;%s;%d\n", "l->aeropuerto->OACI","l->aeropuerto->codigoLocal", "l->aeropuerto->descripcion", l->aeropuerto->totalDespegues + l->aeropuerto->totalAterrizajes);
 		l=l->sig;
 	}
-	fclose(fd);
+
+	fclose(fd); //cierra el stream 
 	return 1;
 }
 
@@ -125,9 +133,37 @@ int movimientosAeropuerto(aeropuertoADT aeropuerto)
 }
 
 
-int movimientosInternacionales()
+int movInternacional(tLista listaDeAeropuertosLocales){
+	int aterrizajes, despegues; 
+	Tlista l=listaDeAeropuertosLocales;
+	FILE * achivoP;//puntero que apunta a un archivo
+	fd=fopen("movs_internacional.csv", "w");//creamos un archivo "movs_in(...)" y recibe "w" que permite la escritura en el archivo
+	
+	//Recorremos la lista 
+	while(l!=NULL){
+		//////////////////////////////////////////////////////////////////////////////
+		//Que son los MOVIMIENTOS INTERNACIONALESS???
+		//////////////////////////////////////////////////////////////////////////////
+
+		//Verificamos que el aeropuerto sea internacional 
+		if(l->aeropuerto->trafico==1){
+
+			//Verificamos que hubo movimiento del aeropuerto en ese año
+			if((despegues= l->aeropuerto->movimientos->totalDespegues !=0) || (aterrizajes=l->aeropuerto->movimientos->totalAterrizajes != 0)){
+				fprintf(archivoP, "%s;%s;%d;%d;%d\n", l->aeropuerto->OACI, l->aeropuerto->IATA, despegues, aterrizajes, despegues+aterrizajes);
+			}
+		}
+
+		l=l->sig; 
+	}
+	fclose(archivoP);//cerramos el stream
+	return 1;
+}
+
+
+int movimientosInternacionales(aeropuertoADT aero)
 {
-	FILE * movsInt = fopen("movs_internacional.csv", "wt");
+	return movInternacional(aero->aeropuertosLocales);
 }
 
 int vuelosPorDía()
