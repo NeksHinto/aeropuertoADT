c38812582a6b60654411a9228d79ae831cdda693
NeksHinto <NeksHinto@users.noreply.github.com>
2018-07-09 13:26:40 -0300

    Update aeropuertoFront.c

--- aeropuertoFront.c
+++ aeropuertoFront.c
@@ -5,9 +5,10 @@
 #include "airportBack.h"
 
 #define	LIMPIA_BUFFER	while(getchar()!='\n')
-#define	CHAR_MAX		100
-#define	DELIM			';'
-#define	SALTO_LINEA		'\n'
+#define	SALTEA_LINEA(c)	while(c!=SALTO_LINEA)
+#define	CHAR_MAX	100
+#define	DELIM		';'
+#define	SALTO_LINEA	'\n'
 
 /*Lee línea del archivo en la que está posicionado el puntero linea*/
 char * leerLinea(char * linea, char * path){
@@ -18,23 +19,18 @@ char * leerLinea(char * linea, char * path){
 	char linea[CHAR_MAX] = {0};
 	//Salteamos la primer linea (Encabezados de las columnas)
 	char c;
-	while(c!=SALTO_LINEA){
-		c = fgetc(archivo); //Devuelve el siguiente char y avanza la posición del puntero al archivo
-	}
+	SALTEA_LINEA(c = fgetc(archivo)); //Devuelve el siguiente char y avanza la posición del puntero al archivo
 	//Copiamos la línea	
 	linea = fgets(linea, CHAR_MAX, archivo);
 
 	return linea;
 }
 
-
-
-
-
-
-
-
-
-
-
-
+/*Mueve el puntero de la línea a la primer posición del token n desde donde está*/
+void moverPtrNCols(char * s, int n)//Validar que esté más adelante y no recorra todo again
+{
+	int i;
+	for(i=0; i<n; i++)
+		RECORRE_TOKENS;
+	return;
+}
