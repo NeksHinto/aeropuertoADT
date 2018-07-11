b01bb678a111d8c5f284742109cdc51da4d4c62e
NeksHinto <NeksHinto@users.noreply.github.com>
2018-07-11 05:56:07 -0300

    Update aeropuertoMAIN.c

--- aeropuertoMAIN.c
+++ aeropuertoMAIN.c
@@ -1,57 +1,83 @@
-aeropuertoADT registro = nuevoRegistroAero();
-	FILE * archivoVuelos;
-	archivoVuelos = fopen("./eana1401-1802.csv", "r");
-	char * lineaVuelos = malloc(CHAR_MAX);
-	char * lineaVuelosAux = malloc(CHAR_MAX);
-	//Guardo la primer linea del archivo vuelos
-	strcpy(lineaVuelos, leerLinea(lineaVuelos, archivoVuelos));
-	strcpy(lineaVuelosAux, lineaVuelos);
-	do{
-		if(validaLinea(lineaVuelosAux)){
-			;
-		}
-		else{
-			int fueraDeAnio=0;
-			char * fecha = malloc(DIAS_MAX);
-			strcpy(fecha, retornaToken(lineaVuelos, FECHA));
-			int dia = fechaADia(fecha, &fueraDeAnio, argv[1]);
-			validaFlag(dia, ERR_DIA);
-			if(!fueraDeAnio){
-				int flag = registraMovDia(registro, dia);
-				validaFlag(flag, ERR_DIA);
-				char clasificacion = (strcmp(retornaToken(lineaVuelos, CLASIFICACION), "Cabotaje")==0?0:1);
-				int aterrizaje = (strcmp(retornaToken(lineaVuelos, TIPO_MOV), "Despegue")==0?0:1);
-				//Tres pos: 1 local y el otro no, viceversa, o ambos locales (vuelos de cabotaje)
-				if(!aterrizaje){
-					strcpy(OACILocal, retornaToken(lineaVuelos, OACI_ORIG));
-					strcpy(OACIAnon, retornaToken(lineaVuelos, OACI_DEST));
-				else{
-					strcpy(OACILocal, retornaToken(lineaVuelos, OACI_DEST));
-					strcpy(OACIAnon, retornaToken(lineaVuelos, OACI_ORIG));
-				}
-				esDesconocido(OACIAnon);
-				char * codigoLocal1 = malloc(LOCAL_MAX);
-				char * IATA1 = malloc(IATA_MAX);
-				char * descripcion1 = malloc(CHAR_MAX);
-				char trafico1;
-				datosAeropuerto(OACILocal, codigoLocal1, IATA1, descripcion1, &trafico1);
-				int flag = agregarAeropuerto(OACILocal, codigoLocal1, descripcion1, trafico1);
-				validaFlag(flag, ERR_AERO);
-				flag = agregarMovimiento(regsitro, OACILocal, OACIAnon, clasificacion, aterrizaje);
-				validaFlag(flag, ERR_MOV);
-				if(clasificacion){
-					char * codigoLocal2 = malloc(LOCAL_MAX);
-					char * IATA2 = malloc(IATA_MAX);
-					char * descripcion2 = malloc(CHAR_MAX);
-					char trafico2;
-					datosAeropuerto(OACIAnon, codigoLocal2, IATA2, descripcion2, &trafico2);
-					int flag = agregarAeropuerto(OACIAnon, codigoLocal2, IATA2, descripcion2, trafico2);
-					validaFlag(flag, ERR_AERO);
-					flag = agregarMovimiento(regsitro, OACIAnon, OACILocal, clasificacion, aterrizaje);
-					validaFlag(flag, ERR_MOV);
-				}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <errno.h>
+#include "aeropuertoFront.h"
+#include "aeropuertoBack.h"
+
+#define RANGO(c)			(strcmp(c, "2014")==0 || strcmp(c, "2015")==0 || strcmp(c, "2016")==0 || strcmp(c, "2017")==0 || strcmp(c, "2018")==0)	
+#define	RECORRE_TOKENS		(strtok(NULL, DELIM_TOKEN))
+#define	BOOLEAN_TOKEN(s)	((RECORRE_TOKENS==(s))?1:0)
+#define	esNA(s)				(strcmp(s, "N/A")==0)
+#define	FECHA_MAX			9
+
+
+int main(int argc, char *argv[])
+{
+	if(argc==2 && RANGO(argv[1])){
+		aeropuertoADT registro = nuevoRegistroAero();
+		FILE * archivoVuelos;
+		archivoVuelos = fopen("./eana1401-1802.csv", "r");
+		char * lineaVuelos = malloc(CHAR_MAX);
+		char * lineaVuelosAux = malloc(CHAR_MAX);
+		//Guardo la primer linea del archivo vuelos
+		strcpy(lineaVuelos, leerLinea(lineaVuelos, archivoVuelos));
+		strcpy(lineaVuelosAux, lineaVuelos);
+		do{
+			if(validaLinea(lineaVuelosAux)){
+				;
 			}
-			lineaVuelos=fgets(lineaVuelos, CHAR_MAX, archivoVuelos);
-		}while(!feof(archivoVuelos));
+			else{
+				int fueraDeAnio=0;
+				char * fecha = malloc(DIAS_MAX);
+				strcpy(fecha, retornaToken(lineaVuelos, FECHA));
+				int dia = fechaADia(fecha, &fueraDeAnio, argv[1]);
+				validaFlag(dia, ERR_DIA);
+				if(!fueraDeAnio){
+					int flag = registraMovDia(registro, dia);
+					validaFlag(flag, ERR_DIA);
+					char clasificacion = (strcmp(retornaToken(lineaVuelos, CLASIFICACION), "Cabotaje"))==0?0:1;
+					int aterrizaje = (strcmp(retornaToken(lineaVuelos, TIPO_MOV), "Despegue"))==0?0:1;
+					//Tres pos: 1 local y el otro no, viceversa, o ambos locales (vuelos de cabotaje)
+					if(!aterrizaje){
+						strcpy(OACILocal, retornaToken(lineaVuelos, OACI_ORIG));
+						strcpy(OACIAnon, retornaToken(lineaVuelos, OACI_DEST));
+					else{
+						strcpy(OACILocal, retornaToken(lineaVuelos, OACI_DEST));
+						strcpy(OACIAnon, retornaToken(lineaVuelos, OACI_ORIG));
+					}
+					esDesconocido(OACIAnon);
+					char * codigoLocal1 = malloc(LOCAL_MAX);
+					char * IATA1 = malloc(IATA_MAX);
+					char * descripcion1 = malloc(CHAR_MAX);
+					char trafico1;
+					datosAeropuerto(OACILocal, codigoLocal1, IATA1, descripcion1, &trafico1);
+					int flag = agregarAeropuerto(OACILocal, codigoLocal1, descripcion1, trafico1);
+					validaFlag(flag, ERR_AERO);
+					flag = agregarMovimiento(regsitro, OACILocal, OACIAnon, clasificacion, aterrizaje);
+					validaFlag(flag, ERR_MOV);
+					if(clasificacion){
+						char * codigoLocal2 = malloc(LOCAL_MAX);
+						char * IATA2 = malloc(IATA_MAX);
+						char * descripcion2 = malloc(CHAR_MAX);
+						char trafico2;
+						datosAeropuerto(OACIAnon, codigoLocal2, IATA2, descripcion2, &trafico2);
+						int flag = agregarAeropuerto(OACIAnon, codigoLocal2, IATA2, descripcion2, trafico2);
+						validaFlag(flag, ERR_AERO);
+						flag = agregarMovimiento(regsitro, OACIAnon, OACILocal, clasificacion, aterrizaje);
+						validaFlag(flag, ERR_MOV);
+					}
+				}
+				lineaVuelos=fgets(lineaVuelos, CHAR_MAX, archivoVuelos);
+			}while(!feof(archivoVuelos));
+			fclose(archivoVuelos);
+		}	
+		return 0;
 	}
-	return 0;
+	else{
+		fprintf(stderr, "Vuelva a ejecutar el programa ingresando un año entre el 2014 y 2018 inclusive.\n");
+		return EXIT_ERR;
+	}
+}
